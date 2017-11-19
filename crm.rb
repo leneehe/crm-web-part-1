35bc618f78561006cceadad795603f64cbf19fa0
Lena <fatmeow@gmail.com>
2017-11-19 15:19:53 -0500

    Complete add invalid id redirection

--- crm.rb
+++ crm.rb
@@ -16,7 +16,11 @@ end
 
 get '/contacts/:id' do
   @contact = Contact.find_by(:id => params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 
